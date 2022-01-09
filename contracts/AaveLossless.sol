299b5adcef9099059776fb4598e774fd50ae1b21
Bibix93 <baraa.elalami@gmail.com>
2022-01-09 06:12:44 +0100

    added comments

--- contracts/AaveLossless.sol
+++ contracts/AaveLossless.sol
@@ -7,32 +7,52 @@ import '../../interfaces/IERC20.sol';
 import './Lossless.sol';
 
 
+
+/** @title AaveLossless
+ *  @dev This contract implement is a lossless betting contracts for football 1X2 that use Aave to generate yield
+ * Contract has an owner to settle the bet, to be replaced by Chainlink.
+ * Process : - Owner creates contract, sets token and lendingpool : token for betting and lending pool to use.
+ *           - Contract can also be sponsored : someone deposit without participating in betting.
+ */
 contract AaveLossless is Lossless  {
 
+    /// token used to bet
     address public token;
+    /// player winning the lottery after game is settled
     address public winner;
+    /// amount of deposit by sponsors
     uint256 public sponsorDeposit;
+    /// team winning
     BetSide public winningSide;
     /// Aave Lending pool to deposit tokens
     ILendingPool public lendingPool;
     enum BetSide {OPEN, HOME, DRAW, AWAY}
     
     
-    
+     /**
+     * @dev Throws if betside is not valid
+     */
     modifier correctBet(BetSide betSide) {
         require(betSide == BetSide.HOME || betSide == BetSide.AWAY|| betSide == BetSide.DRAW, 'invalid argument for bestide');
         _;
     }
-    constructor(address _token, address _lendingPoolAddress, uint256 _matchStartBlock, uint256 _matchFinishBlock) public  Lossless(_matchStartBlock,_matchFinishBlock ){
+
+    /**
+     * @dev Initialize the contract settings : matchStartBlock, matchFinishBlock, token and lending pool.
+     */
+    constructor(address _token, address _lendingPoolAddress, uint256 _matchStartBlock, uint256 _matchFinishBlock)
+    public  
+    Lossless(_matchStartBlock, _matchFinishBlock ){
         status = MatchStatus.OPEN;
         token = _token;
         lendingPool = ILendingPool(_lendingPoolAddress);
         winningSide = BetSide.OPEN;
         winner = address(0);
-
-
     }
 
+    /**
+     * @dev Sponsor the contract, funds received will be used to generate yield.
+     */
     function sponsor(uint256 amount) public payable isOpen() {
         require(amount > 0, 'amount must be positif');
         uint256 allowance = IERC20(token).allowance(msg.sender, address(this));
@@ -46,6 +66,10 @@ contract AaveLossless is Lossless  {
 
 
     }
+
+    /**
+     * @dev Places the bet.
+     */
     function placeBet(BetSide betSide, uint256 amount) public payable isOpen() correctBet(betSide) {
         require(amount > 0, 'amount must be positif');
         if (betSide==BetSide.HOME) {
@@ -67,7 +91,9 @@ contract AaveLossless is Lossless  {
 
     }
     
-
+    /**
+     * @dev Withdraw balance after game is over.
+     */
     function withdraw() public isPaid() {
 
         require(playerBalance[msg.sender]>0 , 'balance is zero');
@@ -77,6 +103,9 @@ contract AaveLossless is Lossless  {
 
     }
 
+    /**
+     * @dev can be called by owner to settle game and withdraw from the pool : To be replaced by chainlink.
+     */
     function setMatchWinnerAndWithdrawFromPool(BetSide _winningSide) public isFinished() onlyOwner() correctBet(_winningSide) {
         require(status == MatchStatus.OPEN, 'Cant settle this match');
         status = MatchStatus.PAID;
@@ -87,6 +116,9 @@ contract AaveLossless is Lossless  {
 
     }
 
+    /**
+     * @dev Internal function to find the winnner of the lottery.
+     */
     function findWinner() internal {
         if(winningSide == BetSide.HOME) {
             winner = findHomeWinner();
@@ -97,6 +129,9 @@ contract AaveLossless is Lossless  {
         }
     }
 
+    /**
+     * @dev Internal function to update the balance of the winnner of the lottery.
+     */
     function payoutWinner() internal  {
         uint256 winnerPayout = IERC20(token).balanceOf(address(this)) - totalDeposits;
         playerBalance[winner] += winnerPayout;
--- contracts/Lossless.sol
+++ contracts/Lossless.sol
@@ -4,56 +4,93 @@ pragma solidity ^0.6.0;
 
 import './access/Ownable.sol';
 
+/** @title Lossless
+ *  @dev This contract implement is a boilerplate for lossless betting contracts for football 1X2.
+ * Contract has an owner to settle the bet, to be replaced by Chainlink.
+ * Process : - Owner creates contract, sets matchStartBlock and matchFinishBlock : Players can place bets before matchStartBlock 
+ * and owner can settle the game after matchFinishBlock.
+ *           - Before matchStartBlock player can pick a side ( Home, Draw, Away), choose an amount and place a bet.
+ *           - After placing a bet the player win points proportional to the 'amount' and 'blocks remaining before matchStartBlock'.
+ *           - The chance of winning for the player is proportional to his points. 
+ *           - The money deposited for bets is then lent to a protocol and earns yield.     
+ *           - After game is finished and owner sets winning team, contract pick randomly a winner who bet on the right side.
+ *           - Winner wins the yield generated, and other players are refunded.
+ */
 
 contract Lossless is Ownable  {
 
     MatchStatus public status;
+    /// block at which game is supposed to start
     uint256 public matchStartBlock;
+    /// block after which game should have ended
     uint256 public matchFinishBlock;
+    /// total amount deposited in the contract
     uint256 public totalDeposits;
+    /// total points earned by players betting home
     uint256 public homePointsTrackers;
+    /// total points earned by players betting draw
     uint256 public drawPointsTrackers;
+    /// total points earned by players betting away
     uint256 public awayPointsTrackers;
+    /// number of home bets
     uint256 public homeBets;
-    uint256 public awayBets;
+    /// number of draw bets
     uint256 public drawBets;
+    /// number of away bets
+    uint256 public awayBets;
+    /// tracks total points earned by player betting home after bet number i, used for computing winner
     uint256 [] public homePointsAfterBet;
+    /// tracks total points earned by player betting draw after bet number i, used for computing winner
     uint256 [] public drawPointsAfterBet;
+    /// tracks total points earned by player betting away after bet number i, used for computing winner
     uint256 [] public awayPointsAfterBet;
+    /// tracks which playerr placed home bet number i
     mapping(uint => address) public homeBetPlacer;
+    /// tracks which playerr placed draw bet number i
     mapping(uint => address) public drawBetPlacer;
+    /// tracks which playerr placed away bet number i
     mapping(uint => address) public awayBetPlacer;
+    /// tracks players deposits
     mapping(address => uint) public playerBalance;
+    /// tracks players home points
     mapping(address => uint) public playerHomePoints;
+    /// tracks players draw points
     mapping(address => uint) public playerDrawPoints;
+    /// tracks players away points
     mapping(address => uint) public playerAwayPoints;
     enum MatchStatus {OPEN, PAID}
 
     
-    
+     /**
+     * @dev Throws if called after game started
+     */
     modifier isOpen() {
         require(status == MatchStatus.OPEN, 'Match not open');
         require(block.number < matchStartBlock, 'Cant place bet now');
         _;
     }
 
-    modifier isRunning() {
-        require(block.number < matchFinishBlock && block.number > matchStartBlock  , 'Game is running');
-        _;
-    }
+     /**
+     * @dev Throws if called before game is Finished 
+     */
 
     modifier isFinished() {
         require(block.number > matchFinishBlock  , 'Game is finished');
         _;
     }
 
+     /**
+     * @dev Throws if called after game is Paid
+     */
     modifier isPaid() {
         require(status == MatchStatus.PAID, 'Match not paid');
         _;
     }
 
     
-    
+    /**
+     * @dev Initialize the contract settings : matchStartBlock and matchFinishBlock.
+     */
      constructor(uint256 _matchStartBlock, uint256 _matchFinishBlock) public {
         status = MatchStatus.OPEN;
         matchStartBlock = _matchStartBlock; 
@@ -61,7 +98,9 @@ contract Lossless is Ownable  {
 
     }
 
-    
+    /**
+     * @dev Places home bet.
+     */
     function placeHomeBet(uint256 amount) internal {
         uint256 currentBlock = block.number;
         uint256 points = amount *  (matchStartBlock - currentBlock);
@@ -73,6 +112,9 @@ contract Lossless is Ownable  {
         
     }
 
+    /**
+     * @dev Places draw bet.
+     */
     function placeDrawBet(uint256 amount) internal {
         uint256 currentBlock = block.number;
         uint256 points = amount *  (matchStartBlock - currentBlock);
@@ -84,6 +126,9 @@ contract Lossless is Ownable  {
         
     }
 
+    /**
+     * @dev Places away bet.
+     */
     function placeAwayBet(uint256 amount) internal {
         uint256 currentBlock = block.number;
         uint256 points = amount * (matchStartBlock - currentBlock);
@@ -96,6 +141,10 @@ contract Lossless is Ownable  {
     }
 
 
+
+    /**
+     * @dev Find home winner.
+     */
     function findHomeWinner() internal returns(address) {
         uint256 random = random();
         random = random % homePointsTrackers;
@@ -121,6 +170,9 @@ contract Lossless is Ownable  {
 
     }
 
+    /**
+     * @dev Find Draw winner.
+     */
     function findDrawWinner() internal returns(address) {
         uint256 random = random();
         random = random % drawPointsTrackers;
@@ -146,6 +198,9 @@ contract Lossless is Ownable  {
 
     }
 
+    /**
+     * @dev Find away winner.
+     */
     function findAwayWinner() internal returns(address) {
         uint256 random = random();
         random = random % awayPointsTrackers;
@@ -171,7 +226,10 @@ contract Lossless is Ownable  {
 
     }
   
-   
+
+    /**
+     * @dev Generate random number to compute the winner, to be replaced with Chainlink VRF.
+     */
     function random() private view returns(uint) {
         return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp ,totalDeposits, matchStartBlock, matchFinishBlock )));
     }
